be15ed03c07c8263362f77006f4131d91ac1db53
Bastien <Bastien91580@yahoo.fr>
2019-03-25 17:11:16 +0100

    Simple Fight

--- src/main.js
+++ src/main.js
@@ -1,18 +1,81 @@
 "use strict";
 exports.__esModule = true;
 var Pokemon = /** @class */ (function () {
-    function Pokemon(name, speed) {
-        this.name = name;
-        this.speed = speed;
-        console.log(this.name);
+    function Pokemon(_name, _speed, _health, _attack, _defence) {
+        this.name = _name;
+        this.speed = _speed;
+        this.health = _health;
+        this.attack = _attack;
+        this.defence = _defence;
     }
+    Pokemon.prototype.launchAttack = function (pokemonExt, random) {
+        var damage = calculateDamages(this.attack, pokemonExt.defence, random);
+        if (damage > 0) {
+            pokemonExt.health -= damage;
+            console.log(this.name + " inflige " + damage + " pts. de dégats à " + pokemonExt.name + ".");
+            return damage;
+        }
+        else {
+            console.log("Ce n'est pas efficace, " + pokemonExt.name + " résiste!");
+            return 0;
+        }
+    };
     return Pokemon;
 }());
 exports.Pokemon = Pokemon;
-function firstAttaque(pokemonA, pokemonB) {
-    if (pokemonA.speed > pokemonB.speed)
+function whoAttaquefirst(pokemonA, pokemonB) {
+    if (pokemonA.speed > pokemonB.speed) {
+        console.log(pokemonA.name + " attaque en premier.");
         return 1;
-    else
+    }
+    else {
+        console.log(pokemonB.name + " attaque en premier.");
         return 2;
+    }
 }
-exports.firstAttaque = firstAttaque;
+exports.whoAttaquefirst = whoAttaquefirst;
+function calculateDamages(_attack, _defence, random) {
+    var powerBase = random;
+    var levelAttack = 1;
+    var damage = Math.floor(Math.floor(Math.floor(2 * levelAttack / 5 + 2) * _attack * powerBase / _defence) / 50) + 2;
+    return damage;
+}
+exports.calculateDamages = calculateDamages;
+function fight(pokemonA, pokemonB, randomA, randomB) {
+    console.log("Le combat entre " + pokemonA.name + " et " + pokemonB.name + " commence !");
+    var first = whoAttaquefirst(pokemonA, pokemonB);
+    var A;
+    var B;
+    if (first === 1) {
+        A = pokemonA;
+        B = pokemonB;
+    }
+    else {
+        A = pokemonB;
+        B = pokemonA;
+    }
+    while (A.health !== 0 && B.health !== 0) {
+        A.launchAttack(B, randomA);
+        if (alive(B) === "ko") {
+            console.log(B.name + " est KO, " + A.name + " est le vainqueur");
+            return 1;
+        }
+        B.launchAttack(A, randomB);
+        if (alive(A) === "ko") {
+            console.log(A.name + " est KO, " + B.name + " est le vainqueur");
+            return 2;
+        }
+    }
+}
+exports.fight = fight;
+function alive(pokemon) {
+    if (pokemon.health <= 0)
+        return "ko";
+    else
+        return "ok";
+}
+exports.alive = alive;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+exports.getRandomInt = getRandomInt;
--- test/pkmnTests.test.js
+++ test/pkmnTests.test.js
@@ -1,7 +1,45 @@
 const Pkmn = require('./../src/main.js');
 
 test('Carapuce vs Salameche -> Salameche (1)', () => {
-	let Carapuce = new Pkmn.Pokemon("Carapuce", 43);
-	let Salameche = new Pkmn.Pokemon("Salameche", 65);
- 	expect(Pkmn.firstAttaque(Salameche,Carapuce)).toBe(1);
+	let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+	let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
+ 	expect(Pkmn.whoAttaquefirst(Salameche, Carapuce)).toBe(1);
 });
+
+test('Carapuce vs Salameche -> Salameche (2)', () => {
+	let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+	let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
+ 	expect(Pkmn.whoAttaquefirst(Carapuce, Salameche)).toBe(2);
+});
+
+test('Carapuce attaque Salameche', () => {
+	let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+	let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
+	// 1. Math.floor(2 * 10 / 5 + 2) -> 2/7 -> 2
+	// 2. Math.floor((1.) * 48 * 10 / 43) -> 22
+	// 3. Math.floor((2.) / 50) + 2; -> 2
+ 	expect(Carapuce.launchAttack(Salameche, 10)).toBe(2);
+});
+
+test('Salameche attaque Carapuce', () => {
+	let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+	let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
+ 	expect(Salameche.launchAttack(Carapuce, 100)).toBe(5);
+ 	//Math.floor(Math.floor(Math.floor(2 * levelAttack / 5 + 2) * _attack * powerBase / _defence) / 50) + 2;
+ 	// 1. Math.floor(2 * 100 / 5 + 2) -> 2/7 -> 2
+	// 2. Math.floor((1.) * 48 * 10 / 43) -> 160
+	// 3. Math.floor((2.) / 50) + 2; -> 5
+});
+
+test('Combat entre Carapuce et Salameche', () => {
+	let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+	let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
+ 	expect(Pkmn.fight(Carapuce, Salameche, 40, 40)).toBe(2);
+
+});
+
+
+
+// constructor(_name, _speed, _health, _attack, _defence)
+//let Carapuce = new Pkmn.Pokemon("Carapuce", 43, 44, 48, 65);
+//let Salameche = new Pkmn.Pokemon("Salameche", 65, 39, 52, 43);
